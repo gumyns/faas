8856066ac96bac3322aaf6e7ede72f4218c11639
Dawid Szydlo <dawid.szydlo@spot.pl>
2017-09-09 12:58:16 +0200

    jpk model SprzedazWiersz

--- jpkModel.py
+++ jpkModel.py
@@ -115,11 +115,52 @@ class Podmiot1(Serializable):
         self.addr = AdresPodmiotu(owner)()
 
 
+class SprzedazWiersz(Serializable):
+    def __init__(self, name):
+        Serializable.__init__(self, name)
+        self.LpSprzedazy = Elem("tns:LpSprzedazy")
+        self.NrKontrahenta = Elem("tns:NrKontrahenta")
+        self.NazwaKontrahenta = Elem("tns:NazwaKontrahenta")
+        self.AdresKontrahenta = Elem("tns:AdresKontrahenta")
+        self.DowodSprzedazy = Elem("tns:DowodSprzedazy")
+        self.DataWystawienia = Elem("tns:DataWystawienia")
+        self.DataSprzedazy = Elem("tns:DataSprzedazy")
+        self.K_10 = Elem("tns:K_10")
+        self.K_11 = Elem("tns:K_11")
+        self.K_12 = Elem("tns:K_12")
+        self.K_13 = Elem("tns:K_13")
+        self.K_14 = Elem("tns:K_14")
+        self.K_15 = Elem("tns:K_15")
+        self.K_16 = Elem("tns:K_16")
+        self.K_17 = Elem("tns:K_17")
+        self.K_18 = Elem("tns:K_18")
+        self.K_19 = Elem("tns:K_19")
+        self.K_20 = Elem("tns:K_20")
+        self.K_21 = Elem("tns:K_21")
+        self.K_22 = Elem("tns:K_22")
+        self.K_23 = Elem("tns:K_23")
+        self.K_24 = Elem("tns:K_24")
+        self.K_25 = Elem("tns:K_25")
+        self.K_26 = Elem("tns:K_26")
+        self.K_27 = Elem("tns:K_27")
+        self.K_28 = Elem("tns:K_28")
+        self.K_29 = Elem("tns:K_29")
+        self.K_30 = Elem("tns:K_30")
+        self.K_31 = Elem("tns:K_31")
+        self.K_32 = Elem("tns:K_32")
+        self.K_33 = Elem("tns:K_33")
+        self.K_34 = Elem("tns:K_34")
+        self.K_35 = Elem("tns:K_35")
+        self.K_36 = Elem("tns:K_36")
+        self.K_37 = Elem("tns:K_37")
+        self.K_38 = Elem("tns:K_38")
+        self.K_39 = Elem("tns:K_39")
+
+
 class JPK(Serializable):
     """
         JPK xml definition
     """
-
     def __init__(self, owner):
         Serializable.__init__(self, "tns:JPK")
         self.etd = Attr("xmlns:etd", "http://crd.gov.pl/xml/schematy/dziedzinowe/mf/2016/01/25/eD/DefinicjeTypy/")
