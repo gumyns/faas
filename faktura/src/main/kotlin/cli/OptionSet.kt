b30ed4d00ced22e918086e9292609d16d43a069a
Dawid Szydlo <dawid.szydlo@spot.pl>
2018-01-14 19:58:10 +0100

    descriptions

--- faktura/src/main/kotlin/cli/OptionSet.kt
+++ faktura/src/main/kotlin/cli/OptionSet.kt
@@ -10,8 +10,8 @@ class OptionSet {
         addOption(Option("owner", true, "The seller of products issued on the invoice."))
         addOption(Option("client", true, "The receiver of the products issued on the invoice."))
         addOption(Option("amount", true, "The amount of the service in invoice."))
-        addOption(Option(null, "product-name", false, "The amount of the service in invoice."))
-        addOption(Option(null, "from-project-json", true, "Json generated from various timesheet"))
+        addOption(Option(null, "product-name", false, "The amount of the service in invoice. Optional when used with 'amount' parameter."))
+        addOption(Option(null, "from-project-json", true, "Json generated from various timesheets"))
         addOption(Option(null, "interactive", false, "Interactive mode"))
       }
   }
